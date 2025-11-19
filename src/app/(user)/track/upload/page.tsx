c91ae6deb1233e8e3371c89c3c51904f292fcc43
vantruyen <levantruyen57@gmail.com>
2025-11-19 23:24:55 +0700

    completed function upload track

---
+++ src/app/(user)/track/upload/page.tsx
@@ -0,0 +1,11 @@
+import UploadTabs from "@/components/track/upload.tabs";
+import { Container } from "@mui/material";
+
+const UpLoadPage = () => {
+  return (
+    <Container>
+      <UploadTabs />
+    </Container>
+  );
+};
+export default UpLoadPage;
